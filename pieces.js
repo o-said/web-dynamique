07a17f125c4814c62302dc71b379f3b78178e6fe
said <ouazirsaid@yahoo.fr>
2024-11-22 21:59:08 +0100

    P1C4

--- pieces.js
+++ pieces.js
@@ -11,10 +11,17 @@ const prixElement = document.createElement('p');
 prixElement.innerText = `prix: ${article.prix}`;
 const categorieElement = document.createElement('p');
 categorieElement.innerText = article.categorie ?? ("aucune categorie ")
+const texteDescription = document.createElement('p');
+texteDescription.innerText = article.description ?? ('Pas de description pour le moment');
+const stockElement = document.createElement('p');
+stockElement.innerText = article.disponibilite ? 'En stock ' : 'Rupture de stock'; 
 //opérateur nullish --> categorieElement.innerText = article.categorie ?? "("aucune categorie ") une valeur est null, et donc qu’elle n’a pas de valeur, ou bien lorsqu’elle est undefined
 const sectionFiches = document.querySelector('.fiches');
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(texteDescription);
+sectionFiches.appendChild(stockElement);
+
 
